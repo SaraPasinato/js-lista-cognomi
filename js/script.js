2df4a5f0f5c943479926a8d37f2d4b66e618828f
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-06 14:41:20 +0200

    ADD script + style

--- js/script.js
+++ js/script.js
@@ -10,4 +10,34 @@
    della lista in cui il nuovo utente si trova
  */
 
-console.log("here");
+//list of surnames
+var arr = ["bianchi", "neri", "rossi", "verdi", "gialli"];
+var msg = "";   //for concat string msg;
+var surname = "";
+var reg = "[0-9]";// regular expression for number validation
+var listElements=document.getElementById("list-elements");
+
+//validation  for string that contain string number and empty string
+do {
+    surname = prompt("inserisci il cognome: ");
+
+} while (!surname && surname.match(reg));
+
+//if passed validation than push surname in arr
+
+arr.push(surname.toLowerCase());
+//debug inline
+console.log("Prima dell'ordinamento:");
+console.table(arr);
+//now, sort 
+arr.sort();
+
+//iteration for   html elments output
+for(var i=0; i<arr.length; i++){
+    msg+="<li>"+ (i+1)+": "+ arr[i]+ "</li> \n";
+}
+
+listElements.innerHTML=msg;
+//debug inline
+console.log("dopo l'ordinamento:");
+console.table(arr);
